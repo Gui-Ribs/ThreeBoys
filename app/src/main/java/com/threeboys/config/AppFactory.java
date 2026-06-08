c0f4718eed5a3cbccdc9dc1dd197d9400bf32989
Gui-Ribs <gr.estudios8@gmail.com>
2026-06-08 06:09:23 -0300

    feat: add dependency injection class

--- app/src/main/java/com/threeboys/config/AppFactory.java
+++ app/src/main/java/com/threeboys/config/AppFactory.java
@@ -1,10 +1,32 @@
 package com.threeboys.config;
 
-public class AppFactory {
+public final class AppFactory {
 
-	private final ConnectionFactory connectionFactory = new ConnectionFactory();
+	private final ConnectionFactory connectionFactory;
+	private final DatabaseConfig databaseConfig;
 
-	public ConnectionFactory connectionFactory() {
+	private AppFactory() {
+		this.connectionFactory = ConnectionFactory.getInstance();
+		this.databaseConfig = DatabaseConfig.getInstance();
+	}
+
+	public static AppFactory getInstance(){
+		return Holder.INSTANCE;
+	}
+
+	public DatabaseConfig getDatabaseConfig() {
+		return databaseConfig;
+	}
+
+	public ConnectionFactory getConnectionFactory() {
 		return connectionFactory;
+	}
+
+	public void validConnection() {
+		connectionFactory.valid();
+	}
+
+	private static class Holder {
+		private static final AppFactory INSTANCE = new AppFactory();
 	}
 }
