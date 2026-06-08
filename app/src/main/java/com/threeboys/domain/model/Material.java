250f52fd90045f9ff886da60aee6c6e6f184554b
LucasOGFS <lucas.ogfs@gmail.com>
2026-06-07 23:12:13 -0300

    Implementação dos atributos dos model Material e Produto

--- app/src/main/java/com/threeboys/domain/model/Material.java
+++ app/src/main/java/com/threeboys/domain/model/Material.java
@@ -4,4 +4,74 @@ public class Material {
 
 	private Long id;
 	private String nome;
+	private int qtde;
+	private String unidadeMedida;
+	private String marca;
+	private double preco;
+	private int estoque;
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQtde() {
+		return qtde;
+	}
+
+	public void setQtde(int qtde) {
+		this.qtde = qtde;
+	}
+
+	public String getUnidadeMedida() {
+		return unidadeMedida;
+	}
+
+	public void setUnidadeMedida(String unidadeMedida) {
+		this.unidadeMedida = unidadeMedida;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public int getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 }
--- app/src/main/java/com/threeboys/domain/model/Produto.java
+++ app/src/main/java/com/threeboys/domain/model/Produto.java
@@ -4,4 +4,65 @@ public class Produto {
 
 	private Long id;
 	private String nome;
+	private String chocolate;
+	private String tipo;
+	private double preco;
+	private int qtde;
+	private String observacao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getChocolate() {
+		return chocolate;
+	}
+
+	public void setChocolate(String chocolate) {
+		this.chocolate = chocolate;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public int getQtde() {
+		return qtde;
+	}
+
+	public void setQtde(int qtde) {
+		this.qtde = qtde;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
 }
