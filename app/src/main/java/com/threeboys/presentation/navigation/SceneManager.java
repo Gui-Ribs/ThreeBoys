9618dd4a67b9a93965f80c670db3f54c88b28731
Gui-Ribs <gr.estudios8@gmail.com>
2026-06-10 18:24:29 -0300

    feat: add new screen

--- app/src/main/java/com/threeboys/presentation/navigation/SceneManager.java
+++ app/src/main/java/com/threeboys/presentation/navigation/SceneManager.java
@@ -2,7 +2,10 @@ package com.threeboys.presentation.navigation;
 
 import com.threeboys.config.AppFactory;
 import com.threeboys.presentation.boundary.ClienteBoundary;
+import com.threeboys.presentation.boundary.MaterialBoundary;
 import com.threeboys.presentation.boundary.PedidoBoundary;
+import com.threeboys.presentation.boundary.ProdutoBoundary;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -16,11 +19,6 @@ public class SceneManager {
 		this.factory = factory;
 	}
 
-	/*public void showPedidos() {
-		PedidoBoundary boundary = new PedidoBoundary(factory., this);
-		changeScreen(boundary.getView(), "Pedidos");
-	}*/
-
 	public void dashboard() {
 		DashboardBoundary boundary = new DashboardBoundary(this);
         changeScreen(boundary.getView(), "Painel");
@@ -30,6 +28,21 @@ public class SceneManager {
         ClienteBoundary boundary = new ClienteBoundary(factory.getClienteControl(), this);
         changeScreen(boundary.getView(), "Clientes");
     }
+
+	public void pedidos() {
+		PedidoBoundary boundary = new PedidoBoundary(factory.getPedidoControl(), this);
+		changeScreen(boundary.getView(), "Pedidos");
+	}
+
+	public void material() {
+		MaterialBoundary boundary = new MaterialBoundary(factory.getMaterialControl(), this);
+		changeScreen(boundary.getView(), "Material");
+	}
+	
+	public void produto() {
+		ProdutoBoundary boundary = new ProdutoBoundary(factory.getProdutoControl(), this);
+		changeScreen(boundary.getView(), "Produto");
+	}
 
 	private void changeScreen(Parent root, String titulo) {
 		Scene scene = stage.getScene();
