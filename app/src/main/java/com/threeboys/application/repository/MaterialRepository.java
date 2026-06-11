442faab805421291f7dbddafaa29dc96163a89f8
Gui-Ribs <gr.estudios8@gmail.com>
2026-06-11 03:17:00 -0300

    fix: crud

--- app/src/main/java/com/threeboys/application/repository/MaterialRepository.java
+++ app/src/main/java/com/threeboys/application/repository/MaterialRepository.java
@@ -5,9 +5,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MaterialRepository {
-    List<Material> findAll();
-    List<Material> findByName(String nome);
-    Material update(Material material);
-    Material save(Material material);
-    void delete(long id);
+	List<Material> findAll();
+	List<Material> findByName(String nome);
+	Material update(Material material);
+	Material save(Material material);
+	void delete(long id);
 }
